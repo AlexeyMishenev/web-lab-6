35994ebbfbf7816d7695be98015949703ee5aa33
Alexey Mishenev <mishenevalex@gmail.com>
2019-05-22 06:24:36 +0300

    Implemented Standalone

--- standalone/src/main/java/ru/ifmo/wst/App.java
+++ standalone/src/main/java/ru/ifmo/wst/App.java
@@ -1,35 +1,47 @@
 package ru.ifmo.wst;
 
+import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
+import com.sun.jersey.api.core.ClassNamesResourceConfig;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 import javax.sql.DataSource;
-import javax.xml.ws.Endpoint;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.glassfish.grizzly.http.server.HttpServer;
 import ru.ifmo.wst.dao.AntibioticsDAO;
+import ru.ifmo.wst.rs.AntibioticResource;
 import ru.ifmo.wst.utils.Configuration;
-import ru.ifmo.wst.ws.AntibioticService;
-import ru.ifmo.wst.ws.TestService;
 
 @Slf4j
 public class App {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     Configuration conf = new Configuration("config.properties");
     String scheme = conf.get("scheme", "http:");
     String host = conf.get("host", "localhost");
-    String port = conf.get("port", "8080");
+    String port = conf.get("port", "8081");
     String baseUrl = scheme + "//" + host + ":" + port;
 
-    String antibioticsServiceName = conf.get("antibiotics.service.name");
-    String antibioticsUrl = baseUrl + "/" + antibioticsServiceName;
+    String appName = conf.get("app.name", "standalone-lab4");
+    String appUrl = baseUrl + "/" + appName;
 
     DataSource dataSource = initDataSource();
+    AntibioticResource.STATIC_DAO = new AntibioticsDAO(dataSource);
+
+    ClassNamesResourceConfig resourceConfig = new ClassNamesResourceConfig(
+        AntibioticResource.class);
+
     log.info("Start application");
-    Endpoint.publish(baseUrl + "/test", new TestService());
-    Endpoint.publish(antibioticsUrl, new AntibioticService(new AntibioticsDAO(dataSource)));
+
+    HttpServer httpServer = GrizzlyServerFactory.createHttpServer(appUrl, resourceConfig);
+    httpServer.start();
+    Runtime.getRuntime().addShutdownHook(new Thread(httpServer::stop));
+
     log.info("Application was successfully started");
+
+    System.in.read();
   }
 
   @SneakyThrows
