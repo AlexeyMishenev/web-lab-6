4ac16b33c607cfe4fdcb117235654057c05cff2a
Alexey Mishenev <mishenevalex@gmail.com>
2019-05-24 05:53:55 +0300

    Added CRUD in Service

--- antibiotic-service/src/main/java/ru/ifmo/wst/rs/AntibioticResource.java
+++ antibiotic-service/src/main/java/ru/ifmo/wst/rs/AntibioticResource.java
@@ -3,8 +3,13 @@ package ru.ifmo.wst.rs;
 import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -63,6 +68,43 @@ public class AntibioticResource {
       @QueryParam("dosage") String dose,
       @QueryParam("additional") String additional) {
     return antibioticsDAO.filter(id, name, method, from, to, dose, additional);
+  }
+
+  @PUT
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.TEXT_PLAIN)
+  @SneakyThrows
+  public String create(
+      @QueryParam("name") String name,
+      @QueryParam("method") String method,
+      @QueryParam("from") Integer from,
+      @QueryParam("to") Integer to,
+      @QueryParam("dosage") String dose,
+      @QueryParam("additional") String additional) {
+    return String.valueOf(antibioticsDAO.create(name, method, from, to, dose, additional));
+  }
+
+  @DELETE
+  @Produces(MediaType.TEXT_PLAIN)
+  @Path("/{id}")
+  @SneakyThrows
+  public String delete(@PathParam("id") long id) {
+    return String.valueOf(antibioticsDAO.delete(id));
+  }
+
+  @POST
+  @Produces(MediaType.TEXT_PLAIN)
+  @Path("/{id}")
+  @SneakyThrows
+  public String update(
+      @QueryParam("id") Long id,
+      @QueryParam("name") String name,
+      @QueryParam("method") String method,
+      @QueryParam("from") Integer from,
+      @QueryParam("to") Integer to,
+      @QueryParam("dosage") String dosage,
+      @QueryParam("additional") String additional) {
+    return String.valueOf(antibioticsDAO.update(id, name, method, from, to, dosage, additional));
   }
 
 }
